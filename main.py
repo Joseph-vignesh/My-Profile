d81266d63a3c897be18622a683d10581eb5e5a29
vignesh <vickyasvickyb@gmail.com>
2024-07-16 15:16:48 +0530

    Add app descriptions images links

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
-col1,col2 = st.columns(2)
+empty_col, col1, col2 = st.columns([0.5, 1.5, 1.9])
 
 with col1:
     st.image("images/profile.png")
@@ -19,4 +20,22 @@ with col2:
 
 content2 = """" Below you can find attached the web apps that i have built feel free to contact me
                 """
-st.write(content2)
+st.subheader(content2)
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
