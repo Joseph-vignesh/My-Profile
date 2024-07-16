da71c51073d5049c94ce987f5837a510f27a7b4d
vignesh <vickyasvickyb@gmail.com>
2024-07-16 12:22:27 +0530

    Day 21: Add descriptions on the new columns

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 col1,col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png",width=300)
+    st.image("images/profile.png")
 
 with col2:
     st.title("Vignesh Baskar")
@@ -16,3 +16,7 @@ with col2:
                """
 
     st.info(content)
+
+content2 = """" Below you can find attached the web apps that i have built feel free to contact me
+                """
+st.write(content2)
