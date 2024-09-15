0cee5e2825afa9800fdb70f9a47228f0f0a5d0e4
vignesh <vickyasvickyb@gmail.com>
2024-09-15 16:55:35 +0530

    changed profile image and updated page

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -4,12 +4,14 @@ from send_email import send_email
 st.header("Contact Me")
 
 with st.form(key="mail_form"):
-    user_email = st.text_input("Your email address")
+    user_email = st.text_input("Your email address",placeholder="i.e: Vik*d***kj.@gmailcom")
+    user_number = st.text_input("Type i n your mobile Number",placeholder="i.e:+91 9*******89")
     raw_message = st.text_area("Cover Your Message Here")
     message = f"""\
 Subject: New email from {user_email}
 
 From: {user_email}
+Number: {user_number}
 {raw_message}
  """
     button = st.form_submit_button()
